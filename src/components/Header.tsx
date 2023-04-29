0f9d78041fb51cc8de6c3338d1463ce4d5457c4e
Muhammad Sulaiman <trendingofyt@gmail.com>
2023-04-29 20:40:57 +0500

    first page done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom"
+import {useState} from 'react';
 
 const Header = (props: any) => {
+  const [menu, setmenu] = useState('-translate-y-full')
   return (
-    <div className="bg-black border-solid borer-b borer-white">
+    <div className="bg-black border-solid borer-b borer-white z-[99999]">
     <div className="flex items-center justify-between">
         <div className="w-full h-full">
-          <div className="px-2 py-1.5 bg-[#000000] flex items-center justify-between">
+          <div className="px-2 py-1.5 bg-[#000000] hidden lg:flex items-center justify-between">
 
             <div className="flex items-center gap-14">
               <div className="flex items-center gap-3">
@@ -34,9 +35,12 @@ const Header = (props: any) => {
             </div>
 
           </div>
-
-
-          <div className="grid grid-cols-[1fr,1fr,1fr,1fr,1fr,1fr,1fr,1fr,1fr] h-full divide-x divide-black">
+         
+         <div className="flex items-center h-full pl-2.5">
+          <button onClick={()=> {setmenu('translate-y-0')}}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-8 h-8"><path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" /></svg></button>
+         </div>
+  
+          <div className={`transition lg:translate-y-0 ${menu} flex flex-col lg:grid lg:grid-cols-[1fr,1fr,1fr,1fr,1fr,1fr,1fr,1fr,1fr] h-screen lg:h-full divide-y lg:divide-x divide-black w-full fixed top-0 left-0 lg:relative lg:top-auto lg:right-auto z-[99] max-h-screen overflow-y-auto`}>
             <a href="#" className="px-2 py-2 h-full bg-red-500 flex items-center justify-center flex-col gap-1 text-center text-[15px] text-white">
               <img src="/images/icon1.jpg" alt="icon" />
               מבצעים
@@ -81,14 +85,17 @@ const Header = (props: any) => {
               <img src="/images/icon9.jpg" alt="icon" />
               כלי בישול
             </a>
-
+ 
+            <button onClick={()=> {setmenu('-translate-y-full')}} className="absolute top-3 right-3 border-none ">
+             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10"><path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+            </button>
 
           </div>
         </div>
         <div className="flex items-center justify-center h-full bg-black px-5"><img src="/images/logo.png" alt="logo" className="w-full h-full object-cover"/></div>
     </div>
     
-    <div className="flex items-center gap-14 py-1.5 px-2">
+    <div className="hidden lg:flex items-center gap-14 py-1.5 px-2">
       <a href="#" className="flex items-center justify-center px-2 py-1.5 bg-green-500 text-white text-[15px] rounded-md font-bold w-fit">לקופה</a>
       <a href="#" className="flex items-center gap-7 text-white font-bold">
        <div>₪ 0.00 סה"כ</div>
